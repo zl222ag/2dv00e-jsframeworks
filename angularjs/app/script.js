57123908cfe0c0891966a0b78ec06d37bea0949c
Zlatko Ladan <zl222ag@student.lnu.se>
2014-10-25 23:17:41 +0200

    CORRECTED, ADDED INDEX FOR FILES
    MUCH NICE ITS NOW REALLY, YES

--- angularjs/app/script.js
+++ angularjs/app/script.js
@@ -1,4 +1,4 @@
 window.addEventListener('load', function () {
 	'use strict';
-	alert('angularJS');
+	alert('AngularJS');
 }, false);
--- backbonejs/app/script.js
+++ backbonejs/app/script.js
@@ -1,4 +1,4 @@
 window.addEventListener('load', function () {
 	'use strict';
-	alert('BackBone.js');
+	alert('Backbone.js');
 }, false);
