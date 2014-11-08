78c605facb1f1f0c8803cf29b17a43c40b38f6ba
Zlatko Ladan <zl222ag@student.lnu.se>
2014-11-08 20:54:00 +0100

    working jQuery To-do Application
    + Has textbox.
    + Create tasks.
    + Remove tasks.
    + Toggle that it is completed (green if complete).

--- jquery/app/script.js
+++ jquery/app/script.js
@@ -1,4 +1,52 @@
-window.addEventListener('load', function () {
+var ZAPP = {
+	wrapper: 0,
+
+	keyDown: function (e) {
+		'use strict';
+		var elem = null;
+		if (e.keyCode !== 13) {
+			return;
+		}
+
+		elem = $(this);
+		ZAPP.addTask(elem.val());
+		elem.val("");
+		return false;
+	},
+
+	addTask: function (a_taskText) {
+		'use strict';
+		if (typeof a_taskText !== 'string') {
+			return;
+		}
+		a_taskText = a_taskText.trim();
+		if (a_taskText.length < 1) {
+			return;
+		}
+
+		var task = $('<li class="alert"> <button type="button" class="btn btn-danger btn-xs"><span class="glyphicon glyphicon-remove" /></button> <button type="button" class="btn btn-success btn-xs"><span class="glyphicon glyphicon-ok" /></button></li>');
+		task.children('.btn-danger').click(ZAPP.remove);
+		task.children('.btn-success').click(ZAPP.done);
+		task.prepend($('<span />').text(a_taskText));
+		ZAPP.wrapper.children('ul').append(task);
+	},
+
+	remove: function () {
+		'use strict';
+		$(this).parent().remove();
+	},
+
+	done: function () {
+		'use strict';
+		$(this).parent().toggleClass('alert-success');
+	}
+};
+
+$(document).ready(function () {
 	'use strict';
-	alert('jQuery');
+	ZAPP.wrapper = $('<div class="container" />');
+	ZAPP.wrapper.append($('<h1 />').text($(document).attr('title')));
+	ZAPP.wrapper.append($('<ul />'));
+	ZAPP.wrapper.append($('<textarea cols="60" rows="3" />').keydown(ZAPP.keyDown));
+	$(document.body).append(ZAPP.wrapper);
 }, false);
